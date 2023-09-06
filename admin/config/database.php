3adcff5617ee3cbb37113fcf7c0e38603384638a
slyrj <slyrj@hotmail.fr>
2023-09-06 21:18:31 +0200

    Création et configuration du fichier database.php

---
+++ admin/config/database.php
@@ -0,0 +1,14 @@
+<?php
+define('ROOT_URL', 'http://localhost:3000/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'garage');
+
+try {
+    // Connexion PDO
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
