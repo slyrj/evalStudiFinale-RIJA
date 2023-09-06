e28d13666d06a55c7b90ee8cb30d8fabc006cdb5
slyrj <slyrj@hotmail.fr>
2023-09-06 14:05:16 +0200

    MAJ pages html + styles +script

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// let navbar = document.querySelector('.header .navbar');
-
-// document.querySelector('#menu-btn').onclick = () => {
-//   navbar.classList.toggle('active');
-// };
-
-// window.onscroll = () => {
-//   navbar.classList.remove('active');
-// };
-
 document
   .querySelectorAll('.about .video-container .controls .control-btn')
   .forEach((btn) => {
